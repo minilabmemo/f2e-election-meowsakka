0949d650a86dfa86381708d0c323a3fc4dc7ee44
minilabmemo <minilabmemo@gmail.com>
2023-12-18 10:52:40 +0800

    Fix: container only use at > 3xl

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./layouts/Header";
 function App() {
   return (
     <div className="bg bg-slate-100 font-inter">
-      <div className="container m-auto ">
+      <div className="3xl:container m-auto ">
         <Header />
         <Content />
       </div>
